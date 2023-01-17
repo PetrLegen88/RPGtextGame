82f804233127570416eefbb0d5f27014b0ee564d
P.Legen <p.legen@post.cz>
2023-01-17 12:56:50 +0100

    Created class Player and Monster and method fight.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-#Test Push kodu
-print("Hello World!")
+import random
 
-print("Blabla")
 
-print("test dev branch")
+def fight():
+    while Monster.HP >= 0 and Player.HP >= 0:               # I dont know why it works with "and" condition instead "or"
+        Monster.HP -= (Player.damage - Monster.defence)
+        print(f"Monster HP: {Monster.HP}")                  # Debug print
+        Player.HP -= (Monster.damage - Player.defence)
+        print(f"Player HP: {Player.HP}")                    # Debug print
+    if Monster.HP <= 0:
+        print("You have killed Monster, what will be your next move?")
+    else:
+        print("Game over")
+
+
+class Player:
+    damage = 7
+    HP = 1000
+    defence = 3
+
+
+class Monster:
+    damage = 5
+    HP = 100
+    defence = 1
+
+
+fight()
