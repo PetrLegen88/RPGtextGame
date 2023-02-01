d401ad3796c82e023687ae0cd35dc3936b619e40
pk <petr.kalat@bizmachine.com>
2023-02-01 20:36:32 +0100

    bring back the main module content so we can run it ...

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from game.characters import Player, Monster
+from game.interaction import fight
 
+monster1 = Monster(10, 100, 5, 5)
+hero = Player("Amazon", 100, damage=10, defence=1)
 
-
+fight(hero, monster1)
